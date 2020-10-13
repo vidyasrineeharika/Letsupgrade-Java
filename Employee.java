313e52ab24759350659fd5add0f0acc9ba2cf932
vidyasri neeharika madduri <56533257+vidyasrineeharika@users.noreply.github.com>
2020-10-13 16:47:39 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,16 @@
+
+public class Employee{
+        int age=23;
+        String name="Saurabh";
+        String city="chennai";
+        public void display(){
+            System.out.println("The name is " + name);
+            System.out.println("Age is "+age);
+            System.out.println("City is "+city);
+        }
+            public static void main(String[] args){
+                Employee n = new Employee();
+                n.display();
+
+        }
+}
