490db5fb5327c165eb597c562722131c252d176a
vidyasri neeharika madduri <56533257+vidyasrineeharika@users.noreply.github.com>
2020-10-13 19:24:12 +0530

    Day 2 Assignment

--- Employee.java
+++ Employee.java
@@ -11,5 +11,6 @@ public class Employee{
             public static void main(String[] args){
                 Employee n = new Employee();
                 n.display();
+                    
         }
 }
